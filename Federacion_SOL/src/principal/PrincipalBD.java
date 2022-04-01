59a46e05aaa6f5bd7577471ba16ca904f4a0c12d
luisdbb <luisdbb@educastur.org>
2022-04-01 13:19:19 +0200

    commit 1 ABRIL

--- Federacion_SOL/src/principal/PrincipalBD.java
+++ Federacion_SOL/src/principal/PrincipalBD.java
@@ -1,41 +1,25 @@
 package principal;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.Scanner;
 
-import entidades.Bronce;
-import entidades.DatosPersona;
-import entidades.Lugar;
-import entidades.Metal;
-import entidades.Oro;
-import entidades.Plata;
-import utils.ConexBD;
-import utils.Datos;
-import utils.Utilidades;
+import entidades.*;
+import utils.*;
 
 public class PrincipalBD {
 
 	public static void main(String[] args) {
+		insertarAtletas();
 		System.out.println("INICIO");
 		Connection conex = null;
 		Statement consulta = null;
 		ResultSet resultado = null;
 		try {
-
-//			insertarLugares();
-
 			conex = ConexBD.establecerConexion();
 			String consultaStr = "SELECT * FROM lugares";
 			if (conex == null)
@@ -47,15 +31,37 @@ public class PrincipalBD {
 				String nombre = resultado.getString(2);
 				String ubicacion = resultado.getString(3);
 				boolean airelibre = resultado.getBoolean(4);
-
-				java.sql.Date fecha = resultado.getDate(4);
-				java.sql.Time hora = resultado.getTime(4);
-				java.time.LocalDate fechadate = fecha.toLocalDate();
-				java.time.LocalTime horatime = hora.toLocalTime();
-				java.time.LocalDateTime fechahora = LocalDateTime.of(fechadate, horatime);
-				System.out.println("id=" + id + "\tfecha:"
-						+ fechahora.format(DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm:ss")));
+				System.out.println("IDLugar:" + id + ", nombre:" + nombre + ", en " + ubicacion
+						+ (airelibre ? " al airelibre." : " a cubierto."));
 			}
+
+			/// Llamar a un procedimiento remoto de la BD
+			/***
+			 * DELIMITER $$
+			 * 
+			 * DROP PROCEDURE IF EXISTS `bdfederacion`.`obtenerNombrePersona` $$ CREATE
+			 * PROCEDURE `bdfederacion`.`obtenerNombrePersona` (IN EMP_ID INT, OUT EMP_FIRST
+			 * VARCHAR(255)) BEGIN SELECT nombre INTO EMP_FIRST FROM personas WHERE id =
+			 * EMP_ID; END $$
+			 * 
+			 * DELIMITER ;
+			 */
+			final String QUERY = "{call obtenerNombrePersona (?, ?)}";
+			CallableStatement stmt = conex.prepareCall(QUERY);
+			Scanner in = new Scanner(System.in);
+			System.out.println("\nDame el id de la persona y te digo su nombre:");
+			int id = in.nextInt();
+			// Enlazar los valores en los parametros del procedure
+			stmt.setInt(1, id); // Esto establece el ID, que es de tipo INT en el procedure SQL
+			// Como el 2º parametro es OUT hay que registrarlo junto a su tipo SQL, en este caso el nombre es String<->VARCHAR
+			stmt.registerOutParameter(2, java.sql.Types.VARCHAR);
+			// Se llama a execute para ejecutar el procedure en la BD.
+			System.out.println("Executando procedure bdfederacion.obtenerNombrePersona de la BD...");
+			stmt.execute();
+			// Obtenemos el nombre del la persona con el método getXXX
+			String nombrePersona = stmt.getString(2);
+			System.out.println("Nombre de la persona con ID:"+id+" es: " + nombrePersona);
+			in.close();
 		} catch (SQLException e) {
 			System.out.println("Se ha producido una Excepcion:" + e.getMessage());
 			e.printStackTrace();
@@ -79,9 +85,9 @@ public class PrincipalBD {
 	public static void insertarAtletas() {
 
 		Connection conex = ConexBD.establecerConexion();
-		String consultaInsertStr1 = "insert into datospersonas(id, nombre, telefono, fechanac, nifnie) values (?,?,?,?,?)";
-		String consultaInsertStr2 = "insert into participantes(id, dorsal, calle) values (?,?,?)";
-		String consultaInsertStr3 = "insert into atletas(idatleta, altura, peso, idparticipante, idpersona) values (?,?,?,?,?,?)";
+		String consultaInsertStr1 = "insert into personas(id, nombre, telefono, fechanac, nifnie) values (?,?,?,?,?)";
+		//String consultaInsertStr2 = "insert into participantes(id, dorsal, calle) values (?,?,?)";
+		String consultaInsertStr3 = "insert into atletas(id, altura, peso, idequipo, idpersona) values (?,?,?,?,?)";
 
 		try {
 
@@ -90,12 +96,20 @@ public class PrincipalBD {
 				pstmt.setLong(1, dp.getId());
 				pstmt.setString(2, dp.getNombre());
 				pstmt.setString(3, dp.getTelefono());
-				java.sql.Date fechaSQL = new java.sql.Date(0); // .getFechaNac();
+				java.sql.Date fechaSQL = java.sql.Date.valueOf(dp.getFechaNac());
 				pstmt.setDate(4, fechaSQL);
-				pstmt.setString(5, "");
+				pstmt.setString(5, dp.getNifnie().mostrar());
 				int resultadoInsercion = pstmt.executeUpdate();
 			}
-
+			PreparedStatement pstmt2 = conex.prepareStatement(consultaInsertStr3);
+			for (Atleta a : Datos.ATLETAS) {
+				pstmt2.setLong(1, a.getId());
+				pstmt2.setFloat(2, a.getAltura());
+				pstmt2.setFloat(3, a.getPeso());
+				pstmt2.setNull(4, java.sql.Types.INTEGER);
+				pstmt2.setLong(5, a.getPersona().getId());
+				int resultadoInsercion = pstmt2.executeUpdate();
+			}
 		} catch (SQLException e) {
 			System.out.println("Se ha producido una SQLException:" + e.getMessage());
 			e.printStackTrace();
@@ -122,7 +136,5 @@ public class PrincipalBD {
 		}
 
 	}
-
-	
 
 }
